f25fa0686dd7127ba0dd7d4165ed44a119fd1a84
Buzz <jure.eres42@gmail.com>
2022-08-06 21:11:35 +0200

    app created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Navbar/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
